d495d57788f4dab038b90bf4a04a21744cce9ad4
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-19 09:08:46 -0400

    test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,11 +4,12 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  var test = 1 + 2 + 3 + 4;
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>MDY114 THIS IS MY HOME PAGE</IonTitle>
+  <IonTitle>MDY114 THIS IS MY HOME PAGE {test}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
