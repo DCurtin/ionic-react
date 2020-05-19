c24468946aeb1931eaefa34032b44aa764749539
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-19 16:05:43 -0400

    test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useState } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, {useState} from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
