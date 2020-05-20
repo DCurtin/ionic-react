35a99844bc5f109735e7098f3632e052e565ff24
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-20 14:01:28 -0400

    test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -107,7 +107,7 @@ function createTransaction(accountId : String)
 }
 
 function authenticate(){
-  var url = 'https://test.salesforce.com/services/oauth2/authorize?response_type=code&client_id=3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm&redirect_uri=http://localhost:8100/&state=init&prompt=login';
+  var url = 'https://test.salesforce.com/services/oauth2/authorize?response_type=code&client_id=3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm&redirect_uri=http://localhost:8100/&state=init&prompt=login&display=touch';
   let browserRef = window.open(url, '_blank', 'location=no');
   browserRef.addEventListener("loadstart", (event: any) => {
     console.log(event.url);
