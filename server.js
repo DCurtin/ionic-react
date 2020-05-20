14e5e474e99b98cdc4b0a6c83274cea4553725bb
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-19 23:24:49 -0400

    Adding cors bypass

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ var pg = require('pg');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 var connectionString = process.env.DATABASE_URL;
 
 var client = new pg.Client(connectionString);
