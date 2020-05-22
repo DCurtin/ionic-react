a2bb1a50c22f96bedd0226e8c3fec49aaaf24dfe
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-22 16:01:02 -0400

    test

--- server.js
+++ server.js
@@ -219,8 +219,7 @@ app.post('/loginServer', function(req, res){
         console.log(err);
         console.log(response);
 
-        //scheduleSessionTimeout(USER_TIMEOUT_IN_MINUTES, token)
-        scheduleSessionTimeout(1, token)
+        scheduleSessionTimeout(USER_TIMEOUT_IN_MINUTES, token)
 
         var userIdentity = {
           'userId' : userInfo.id,
