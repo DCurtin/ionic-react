f8151d8797dd9b5c717db75cd502380fae1dfbe4
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-22 13:40:13 -0400

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/*',(req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 /*app.get("*", function(req, res) {
